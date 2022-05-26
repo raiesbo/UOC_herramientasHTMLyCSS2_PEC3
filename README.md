21b708869656cd8caad335910e38c4c9a3741ab7
Raimon Espasa <raimon.espasa.bou@webgears-group.com>
2022-05-26 12:56:16 +0200

    Reset repo to base boilerplate

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-[Demo version](https://herramientashtmlycss2-pec3-raiesbo.netlify.app/)
-
-
 # UOC Boilerplate
 
 UOC Boilerplate is a starter template for the HTML and CSS Tools courses from the [Master's Program in Multimedia Applications](https://estudis.uoc.edu/ca/masters-universitaris/aplicacions-multimedia/presentacio) and the [Master's Program in Web App and Website Development](https://estudis.uoc.edu/ca/masters-universitaris/desenvolupament-llocs-aplicacions-web/presentacio) at the [Universitat Oberta de Catalunya](https://www.uoc.edu). It aims to provide a basic, modern frontend web development starter pack based on Parcel and including a Sass compiler, an ES6 transpiler, minifiers, an image transformer, and development tools.
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,37 +6,11 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
-import * as bootstrap from 'bootstrap';
-
 /**
  * Write any other JavaScript below
  */
 
-const daysTag = document.querySelector('.days__time');
-const hoursTag = document.querySelector('.hours__time');
-const minutesTag = document.querySelector('.min__time');
-const secondsTag = document.querySelector('.sec__time');
-
-const timeHandler = () => {
-  let lastDay = new Date('2022-07-08');
-  let now = new Date();
-
-  const diffTime = lastDay - now
-  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-  const diffHours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
-  const diffSeconds = Math.floor((diffTime % (1000 * 60)) / 1000);
-
-  daysTag.textContent = diffDays;
-  hoursTag.textContent = ensureDoubleDigit(diffHours);
-  minutesTag.textContent = ensureDoubleDigit(diffMinutes);
-  secondsTag.textContent = ensureDoubleDigit(diffSeconds);
-}
-
-const ensureDoubleDigit = (num) => {
-  return num < 10 ? `0${num}` : num;
-};
-
-setInterval(() => {
-  timeHandler();
-}, 1000);
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
