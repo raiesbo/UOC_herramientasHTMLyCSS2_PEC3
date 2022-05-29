45573081ed975fb022c86269672b53f8d12363eb
raiesbo <raiesbo@gmail.com>
2022-05-29 22:16:20 +0200

    Finish homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      spacing: {
+        '1': '5px',
+        '2': '10px',
+        '3': '15px',
+        '4': '20px',
+        '5': '25px',
+        '6': '30px',
+        '6': '50px',
+        '7': '60px',
+        '8': '90px',
+        '9': '120px'
+      },
+      screens: {
+        'tablet': '640px',
+        'desktop': '1280px'
+      }
+    },
   },
   plugins: [],
 }
